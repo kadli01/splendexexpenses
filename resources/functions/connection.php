80b5ad3a060d379ca67f47272f8fc16c716c2be1
Helbich Zsolt <helbich.zsolt@gmail.com>
2017-11-20 16:24:04 +0100

    first push

---
+++ resources/functions/connection.php
@@ -0,0 +1,11 @@
+<?php
+	try {
+		$db = new PDO('mysql:host=localhost;dbname=splendex_expenses', 'root', 'admin');
+		$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); // all errors should be handled as an exception
+	}catch(Exception $e)
+	{
+		echo $e->getMessage();
+	    exit;
+	}
+
+?>
---
+++ resources/functions/login.php
@@ -0,0 +1,31 @@
+<?php 
+	session_start();
+	if(isset($_POST["bttLogin"]))
+		{
+			require('connection.php');
+
+			$email = $_POST["email"];
+			$password = $_POST["password"];
+
+			$email_result = $db->prepare("SELECT email FROM users WHERE email = ?");
+		    $email_result->bindParam(1, $email, PDO::PARAM_STR);
+		    $email_result->execute();
+		    $email_item = $email_result->fetch();
+
+		    $password_result = $db->prepare("SELECT password FROM users WHERE email = ?");
+		    $password_result->bindParam(1, $email, PDO::PARAM_STR);
+		    $password_result->execute();
+		    $password_item = $password_result->fetch();
+
+			
+			
+			if($email_item[0] == $email && password_verify($password, $password_item[0]))
+			{
+				header('Location: ../views/account.html');
+			}else {
+				header('Location: ../views/index.php');
+
+			}
+
+		}
+?>
