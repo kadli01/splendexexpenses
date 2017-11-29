550888b410d0c3d81117ca7df9b6d05cc323bc71
Krisztian Kadlicsko <krisztian.kadlicsko@gmail.com>
2017-11-29 13:26:05 +0100

    .

--- resources/functions/config.php
+++ resources/functions/config.php
@@ -7,21 +7,5 @@ $config['maxFileSize'] = 5000000;
 $config['app_url'] = '/splendexexpenses';
 $config = (object) $config;
 
-
-
-// function settings($index){
-// 	if ($config[$index]) {
-// 		return $config[$index];
-// 	} else {
-// 		return false;
-// 	}
-// }
-
-
- $GLOBALS['config'] =  $config;
-// $host = 'localhost';
-// $dbname = 'splendex_expenses';
-// $user = 'root';
-// $pass = 'admin';
-
+$GLOBALS['config'] =  $config;
 ?>
