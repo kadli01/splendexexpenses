ce7503195137c7be8d118593e775ae29e9ad4a06
Helbich Zsolt <helbich.zsolt@gmail.com>
2017-11-29 13:25:37 +0100

    bugfix

--- resources/functions/config.php
+++ resources/functions/config.php
@@ -2,7 +2,7 @@
 $config['host'] = 'localhost';
 $config['dbname'] = 'splendex_expenses';
 $config['user'] = 'root';
-$config['pass'] = '123456';
+$config['pass'] = 'admin';
 $config['maxFileSize'] = 5000000; 
 $config['app_url'] = '/splendexexpenses';
 $config = (object) $config;
--- resources/functions/login.php
+++ resources/functions/login.php
@@ -10,7 +10,7 @@
 			}
 
 
-			$emailResult = $db->prepare("SELECT email FROM users WHERE email = ?");
+			$emailResult = $GLOBALS['db']->prepare("SELECT email FROM users WHERE email = ?");
 		    $emailResult->bindParam(1, $email, PDO::PARAM_STR);
 		    $emailResult->execute();
 		    $emailItem = $emailResult->fetch();
@@ -18,26 +18,26 @@
 		    	$_SESSION['user_email'] = $emailItem[0];
 		    }
 
-		    $passwordResult = $db->prepare("SELECT password FROM users WHERE email = ?");
+		    $passwordResult = $GLOBALS['db']->prepare("SELECT password FROM users WHERE email = ?");
 		    $passwordResult->bindParam(1, $email, PDO::PARAM_STR);
 		    $passwordResult->execute();
 		    $passwordItem = $passwordResult->fetch();
 
-			$idResult = $db->prepare("SELECT user_id FROM users WHERE email = ?");
+			$idResult = $GLOBALS['db']->prepare("SELECT user_id FROM users WHERE email = ?");
 			$idResult->bindParam(1, $email, PDO::PARAM_STR);
 			$idResult->execute();
 			$userId = $idResult->fetch();
-			if(isset($userId[0])){
-				$_SESSION['user_id'] = $userId[0];
-			}
-		
-
 			
+					
 			
 			if($emailItem[0] == $email && password_verify($password, $passwordItem[0]))
 			{
+				if(isset($userId[0])){
+					$_SESSION['user_id'] = $userId[0];
+				}
 				header('Location:'. $config->app_url . '/account.php');
 			}else {
 				$_SESSION['loginError'] = "Wrong e-mail or password!";
+				return false;
 			}
 		}
